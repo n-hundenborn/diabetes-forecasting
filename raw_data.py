1df74cea336e2195a726e61731442a8a42cfa035
cdckr <celina.decker@online.de>
2023-01-29 17:23:45 +0100

    Update: URL Data

--- raw_data.py
+++ raw_data.py
@@ -1,10 +1,6 @@
 # zip files: https://towardsdatascience.com/dont-download-read-datasets-with-url-in-python-8245a5eaa919
 
 
-
-
-
-
 import pandas as pd
 from io import StringIO, BytesIO, TextIOWrapper
 from zipfile import ZipFile
@@ -20,32 +16,20 @@ CSV_FILE = "2011.csv"
 resp = urllib.request.urlopen(ZIP_URL)
 # req = requests.get(ZIP_URL)
 
-print(resp)
 
 
 # reading and storing zip file content
 # zipfile = ZipFile(BytesIO(req.content))
-zipfile = ZipFile(BytesIO(resp.read()))
 
-# directory = "\raw data"
+zipfile = ZipFile(BytesIO(resp.read()), "r")
+
+directory = "\raw data"
 
 
-# with zipfile as zip_ref:
-#     zip_ref.extract(CSV_FILE,directory)
-# pd.read_csv(directory + CSV_FILE)
+with zipfile as zip_ref:
+    zip_ref.extract(CSV_FILE,directory)
+pd.read_csv(directory + CSV_FILE)
 
 
 # data = TextIOWrapper(zipfile.open(CSV_FILE), encoding = "utf-8")
-# print("data:" , data)
 # df = pd.read_csv(data)
-# print("df:" , df)
-
-
-# 
-
-
-# re = urlopen(ZIP_URL)
-# files_zip = zipfile.Zipfile(BytesIO(re.read()))
-
-# df = pd.read_csv(FILE)
-
