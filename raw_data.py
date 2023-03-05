9b1263b4d55e459d1ee3a1cea59f4ae0e922cffa
nhundenborn <nick.hundenborn@protonmail.com>
2023-03-05 12:57:38 +0100

    Add tqdm and type hints

--- raw_data.py
+++ raw_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import random
+from tqdm import tqdm
 
 FOLDER_PATH = "./data/"
 
@@ -39,7 +40,7 @@ feature_list = [
 ]
 
 
-def save_file_from_cloud(file):
+def save_file_from_cloud(file: str) -> int:
     # in case you only have the pkl file but not the csv file
     if os.path.exists(FOLDER_PATH + file[:-3] + "pkl"):
         print("You already have this file's corresponding .pkl file. No need to download the .csv file again.")
@@ -68,7 +69,7 @@ def save_file_from_cloud(file):
 
 
 # fetch and download elements from input list from the google cloud folder
-def save_all_files(files=files_to_load):
+def save_all_files(files: list = files_to_load) -> None:
     print(f"Get a coffee ;) \nDownloading and saving {len(files)} big files will take between {2 * len(files)} and {4 * len(files)} minutes.")
     
     # just in case data folder does not exist, create it
@@ -76,28 +77,28 @@ def save_all_files(files=files_to_load):
         os.mkdir(FOLDER_PATH)
     
     finished_downloads = 0
-    for file in files:
+    for file in tqdm(files):
         finished_downloads += save_file_from_cloud(file)
     print(f"You successfully downloaded {finished_downloads} of the {len(files)} files.")
 
 
 # concats all csv files from the data folder to one dataframe and returns it (using an inner join on the df columns)
-def concat_df():
+def concat_df() -> pd.DataFrame:
     df_list = []
     
-    for file in os.listdir(FOLDER_PATH):
+    for file in tqdm(os.listdir(FOLDER_PATH)):
         if not file.endswith(".pkl"):
             continue
-        print(f"Reading file: {file}")
+        # print(f"Reading file: {file}")
         df_list.append(pd.read_pickle(FOLDER_PATH + file))
     
     return pd.concat(df_list, axis=0, ignore_index=True)
 
 # concats all csv files from the data folder to one dataframe and returns it (using an inner join on the df columns)
-def concat_sampled_df(sample_size = 10000):
+def concat_sampled_df(sample_size:int = 10000):
     df_list = []
     
-    for file in os.listdir(FOLDER_PATH):
+    for file in tqdm(os.listdir(FOLDER_PATH)):
         if not file.endswith(".pkl"):
             continue
 
